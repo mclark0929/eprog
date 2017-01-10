50bd0f737d237a878801c84466ac7a9294c34fe7
Mdoobie <mclark0929@yahoo.com>
2017-01-10 09:36:56 +0800

    Create The Ones.c

---
+++ The Ones.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	char str[5]="01011";
+	int i, count=0;
+	for(i=0; i<sizeof(str); i++){
+		printf("%c\n", str[i]);
+		if(str[0] == '1', count++);
+	}		
+		printf("%d",count++);
+}
