7af835da522f1bd7112d921e606bc277d31ba57d
Mdoobie <mclark0929@yahoo.com>
2017-01-10 09:47:09 +0800

    Create The One.c

---
+++ The One.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+	int i;
+		
+	printf("Enter number here - ");
+	scanf("%d",&i);
+	
+	if(i%2 == 0);
+	printf("%d",i/2);{
+	if(i%2  );
+	printf("%d",3*i+1);
+	}
+}
